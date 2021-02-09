d489e8af9d1cc25425605f8944cdef3250582b77
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-02-09 12:54:57 -0500

    Added open dialog button and listener

--- src/com/hunterltd/ServerWrapper/GUI/WrapperGUI.java
+++ src/com/hunterltd/ServerWrapper/GUI/WrapperGUI.java
@@ -8,33 +8,29 @@ import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 public class WrapperGUI extends JFrame {
-    private JTabbedPane consolePane;
-    private JPanel rootPanel;
-    private JTextArea consoleTextArea;
-    private JTextField commandTextField;
+    private FileDialog serverInfo;
+    private JButton openDialogButton;
+    private JButton runButton;
     private JButton sendButton;
+    private JPanel rootPanel;
     private JScrollPane consoleScrollPane;
+    private JTabbedPane consolePane;
+    private JTextField commandTextField;
+    private JTextArea consoleTextArea;
+    private JTextField serverPathTextField;
     private MinecraftServer server;
 
     public WrapperGUI() {
         add(rootPanel);
-        consoleTextArea.setEditable(false);
-        consoleTextArea.setLineWrap(true);
         ((DefaultCaret) consoleTextArea.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE); // Automatic scrolling
 
-        FileDialog fd = new FileDialog(this, "Select your server.jar", FileDialog.LOAD);
-        fd.setFilenameFilter((dir, name) -> name.endsWith(".jar"));
-        fd.setVisible(true);
-        String filename = fd.getFile();
-        if (filename == null) {
-            System.exit(0);
-        }
         try {
-            server = new MinecraftServer(fd.getDirectory(), filename, 4096, 4096);
+            server = new MinecraftServer(serverInfo.getDirectory(), serverInfo.getFile(), 4096, 4096);
         } catch (IOException e) {
             //TODO: dialog that the server couldn't be opened
             e.printStackTrace();
@@ -45,6 +41,8 @@ public class WrapperGUI extends JFrame {
         Executors.newSingleThreadExecutor().submit(gobbler);
 
         sendButton.addActionListener(e -> sendCommand(consoleTextArea.getText()));
+        openDialogButton.addActionListener(e -> selectNewFile());
+
         consolePane.registerKeyboardAction(e -> {
                 sendCommand(commandTextField.getText());
                 commandTextField.setText("");
@@ -59,6 +57,14 @@ public class WrapperGUI extends JFrame {
             //TODO: dialog that the command couldn't be sent
             ioException.printStackTrace();
         }
+    }
+
+    private void selectNewFile() {
+        FileDialog fd = new FileDialog(this, "Select your server.jar", FileDialog.LOAD);
+        fd.setFilenameFilter((dir, name) -> name.endsWith(".jar"));
+        fd.setVisible(true);
+        serverPathTextField.setText(Paths.get(fd.getDirectory(), fd.getFile()).toString());
+        serverInfo = fd;
     }
 
     public MinecraftServer getServer() {
