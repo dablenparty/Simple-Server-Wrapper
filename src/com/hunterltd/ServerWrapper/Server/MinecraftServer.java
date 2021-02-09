f4be40b519c2c33c51284a4d4f72fd5d01ccbdb3
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-02-08 20:47:25 -0500

    Changed to server launch command and added process getter

--- src/com/hunterltd/ServerWrapper/Server/MinecraftServer.java
+++ src/com/hunterltd/ServerWrapper/Server/MinecraftServer.java
@@ -2,19 +2,26 @@ package com.hunterltd.ServerWrapper.Server;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class MinecraftServer {
-    private Process serverProcess;
+    private final Process serverProcess;
 
-    public MinecraftServer(String serverPath, int initialHeap, int maxHeap) {
+    public MinecraftServer(String serverFolder, String serverFilename, int initialHeap, int maxHeap) throws IOException {
         ProcessBuilder pB = new ProcessBuilder();
-        pB.directory(new File("./"));
-//        String opSys = System.getProperty("os.name"); // TODO: OS compatibility
-        pB.command("cmd.exe", "/c", "echo",  "hello from the server!");
-        try {
-            serverProcess = pB.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        pB.directory(new File(serverFolder));
+
+        pB.command("java",
+                String.format("-Xmx%dM", initialHeap),
+                String.format("-Xms%dM", maxHeap),
+                "-jar",
+                Paths.get(serverFolder, serverFilename).toString(),
+                "nogui");
+
+        serverProcess = pB.start();
+    }
+
+    public Process getServerProcess() {
+        return serverProcess;
     }
 }
