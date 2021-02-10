eed4e890732446a64d2a85a05496b39b017b9ea6
dablenparty <hdavenport8902@gmail.com>
2021-02-10 11:20:14 -0500

    Added static execute method for making a new Thread Executor

--- src/com/hunterltd/ServerWrapper/Server/StreamGobbler.java
+++ src/com/hunterltd/ServerWrapper/Server/StreamGobbler.java
@@ -3,6 +3,7 @@ package com.hunterltd.ServerWrapper.Server;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 public class StreamGobbler implements Runnable {
@@ -17,5 +18,10 @@ public class StreamGobbler implements Runnable {
     @Override
     public void run() {
         new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
+    }
+
+    public static void execute(InputStream inputStream, Consumer<String> consumer) {
+        StreamGobbler gobbler = new StreamGobbler(inputStream, consumer);
+        Executors.newSingleThreadExecutor().submit(gobbler);
     }
 }
