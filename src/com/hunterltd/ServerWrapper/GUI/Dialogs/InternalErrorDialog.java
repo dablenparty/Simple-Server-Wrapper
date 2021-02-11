1f946ecedc9fedcf5ac887cd75af213a8042bb26
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-02-11 16:02:12 -0500

    Added the InternalErrorDialog class

---
+++ src/com/hunterltd/ServerWrapper/GUI/Dialogs/InternalErrorDialog.java
@@ -0,0 +1,7 @@
+package com.hunterltd.ServerWrapper.GUI.Dialogs;
+
+public class InternalErrorDialog extends InfoDialog {
+    public InternalErrorDialog() {
+        super("Internal Error", "An unknown internal error occurred.");
+    }
+}
