766fe0a23b02cd04ecfe339c55605973fa5186b9
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-03-02 14:16:21 -0500

    Moved to an abstract table model

--- src/com/hunterltd/ServerWrapper/Server/Properties/PropertiesTableModel.java
+++ src/com/hunterltd/ServerWrapper/Server/Properties/PropertiesTableModel.java
@@ -1,12 +1,41 @@
 package com.hunterltd.ServerWrapper.Server.Properties;
 
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
-public class PropertiesTableModel extends DefaultTableModel {
+public class PropertiesTableModel extends AbstractTableModel {
     private final ServerProperties properties;
+    private final Object[][] data;
 
     public PropertiesTableModel(ServerProperties props) {
         properties = props;
-        properties.keySet().iterator().forEachRemaining(key -> this.addRow(new Object[]{key, properties.get(key)}));
+        data = new Object[properties.size()][2];
+        int idx = 0;
+        for (Object key :
+                properties.keySet()) {
+            data[idx][0] = key;
+            data[idx][1] = properties.get(key);
+            idx++;
+        }
+    }
+
+    @Override
+    public int getRowCount() {
+        return properties.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return data[rowIndex][columnIndex];
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        data[rowIndex][columnIndex] = aValue;
     }
 }
