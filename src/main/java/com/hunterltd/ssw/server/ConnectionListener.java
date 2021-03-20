f6afa44c9bbcf1deea2f73c54874561f5989f46a
dablenparty <hdavenport8902@gmail.com>
2021-03-20 00:39:08 -0400

    Used a readable boolean value to see if a connection was accepted from outside of this class

--- src/main/java/com/hunterltd/ssw/server/ConnectionListener.java
+++ src/main/java/com/hunterltd/ssw/server/ConnectionListener.java
@@ -2,20 +2,25 @@ package com.hunterltd.ssw.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousCloseException;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 
 public class ConnectionListener {
+    private static AsynchronousServerSocketChannel listener;
+    private static boolean connectionAttempted = false;
+
     public static void start(int port) throws IOException {
-        final AsynchronousServerSocketChannel listener =
-                AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port));
+        connectionAttempted = false;
+        listener = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port));
 
         listener.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
             @Override
             public void completed(AsynchronousSocketChannel result, Void attachment) {
                 listener.accept(null, this);
-                System.out.println("Connection accepted");
+                System.out.println("Someone attempted to connect to the server. Launching it...");
+                connectionAttempted = true;
                 if (result.isOpen()) {
                     try {
                         result.close();
@@ -27,9 +32,27 @@ public class ConnectionListener {
 
             @Override
             public void failed(Throwable exc, Void attachment) {
-                System.err.println("uhhh something went bad\n");
-                exc.printStackTrace();
+
+                if (exc instanceof AsynchronousCloseException) {
+                    System.out.println("Listener successfully closed");
+                } else {
+                    System.err.println("An error occurred closing the listener\n|\nV"); // shows an arrow pointing down
+                    exc.printStackTrace();
+                }
+
             }
         });
+    }
+
+    public static void stop() {
+        try {
+            listener.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isConnectionAttempted() {
+        return connectionAttempted;
     }
 }
