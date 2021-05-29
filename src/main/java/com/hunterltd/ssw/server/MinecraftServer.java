c12eb7f9baafa3efe4995b822995ebdca9b61e01
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-05-29 17:30:49 -0400

    MinecraftServer.java

--- src/main/java/com/hunterltd/ssw/server/MinecraftServer.java
+++ src/main/java/com/hunterltd/ssw/server/MinecraftServer.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Minecraft server wrapper class
+ */
 public class MinecraftServer {
     private Process serverProcess;
     private final ProcessBuilder pB;
@@ -27,6 +30,11 @@ public class MinecraftServer {
     private boolean shouldBeRunning;
     private boolean shouldRestart = false;
 
+    /**
+     * Creates a class from an archive file and settings class
+     * @param serverFile Server .jar file
+     * @param serverSettings Server settings
+     */
     public MinecraftServer(File serverFile, Settings serverSettings) {
         this(serverFile.getParent(), serverFile.getName(), serverSettings);
     }
@@ -52,32 +60,52 @@ public class MinecraftServer {
         pB.command(serverArgs);
 
         commandHistory = new ArrayList<>();
-        commandHistory.add("");
+        commandHistory.add(""); // Not entirely sure why this is needed, but the command history won't work without it
     }
 
+    /**
+     * @return Server process
+     */
     public Process getServerProcess() {
         return serverProcess;
     }
 
+    /**
+     * Sends a command to tge server process
+     * @param cmd Command
+     * @throws IOException if an I/O error occurs writing to the server process
+     */
     public void sendCommand(String cmd) throws IOException {
         if (!cmd.endsWith("\n")) {
             cmd += "\n";
         }
         OutputStream out = serverProcess.getOutputStream();
         out.write(cmd.getBytes(StandardCharsets.UTF_8));
-        out.flush();
+        out.flush(); // sends the command
         commandHistory.add(cmd.trim());
     }
 
+    /**
+     * Updates the classes properties and starts the server
+     * @throws IOException if an I/O error occurs starting the server process
+     */
     public void run() throws IOException {
         propsExists = updateProperties();
         serverProcess = pB.start();
     }
 
+    /**
+     * Sends the stop command to the server
+     * @throws IOException if an I/O error occurs writing to the server process
+     */
     public void stop() throws IOException {
         sendCommand("stop");
     }
 
+    /**
+     * Generates a launch script for the server with the corresponding file extension
+     * @param ext File extension
+     */
     public void generateBatch(String ext) {
         updateExtraArgs();
         String command = String.join(" ", serverArgs);
@@ -90,17 +118,14 @@ public class MinecraftServer {
                         "Overwrite launch file",
                         JOptionPane.YES_NO_OPTION
                 );
-                if (result == JOptionPane.NO_OPTION) {
-                    return;
-                }
+                if (result == JOptionPane.NO_OPTION) return;
             }
 
-            if (ext.equals("bat")) {
-                command += "\npause";
-            }
+            if (ext.equals("bat")) command += "\npause";
             writer.write(command);
             writer.flush();
         } catch (IOException e) {
+            e.printStackTrace();
             new InternalErrorDialog(e);
         } finally {
             InfoDialog dialog = new InfoDialog("File created",
@@ -110,30 +135,54 @@ public class MinecraftServer {
         }
     }
 
+    /**
+     * @return Boolean on whether the server should be running
+     */
     public boolean shouldBeRunning() {
         return shouldBeRunning;
     }
 
+    /**
+     * Sets whether the server should be running or not
+     * @param shouldBeRunning Value to assign to
+     */
     public void setShouldBeRunning(boolean shouldBeRunning) {
         this.shouldBeRunning = shouldBeRunning;
     }
 
+    /**
+     * @return Boolean on whether the server is running
+     */
     public boolean isRunning() {
         return serverProcess.isAlive();
     }
 
+    /**
+     * @return The servers settings
+     */
     public Settings getServerSettings() {
         return serverSettings;
     }
 
+    /**
+     * @return The server path as a {@link Path} object
+     */
     public Path getServerPath() {
         return serverPath;
     }
 
+    /**
+     * @return The servers properties
+     */
     public ServerProperties getProperties() {
         return properties;
     }
 
+    /**
+     * The properties don't always exist (e.g. on a servers first run) so this tries to read the properties file if it
+     * exists
+     * @return Boolean on whether the properties were updated or not
+     */
     public boolean updateProperties() {
         try {
             properties = new ServerProperties(Paths.get(String.valueOf(serverPath.getParent()), "server.properties").toFile());
@@ -150,6 +199,9 @@ public class MinecraftServer {
         }
     }
 
+    /**
+     * Updates the extra args for the server
+     */
     public void updateExtraArgs() {
         serverArgs = new ArrayList<>(Arrays.asList("java",
                 String.format("-Xmx%dM", serverSettings.getMemory()),
@@ -161,26 +213,46 @@ public class MinecraftServer {
         if (serverSettings.hasExtraArgs()) serverArgs.addAll(3, serverSettings.getExtraArgs());
     }
 
+    /**
+     * @return Boolean on whether the properties file exists
+     */
     public boolean propertiesExists() {
         return propsExists;
     }
 
+    /**
+     * @return Boolean on whether the server should restart
+     */
     public boolean shouldRestart() {
         return shouldRestart;
     }
 
+    /**
+     * Sets whether the server should restart or not
+     * @param shouldRestart Value to set
+     */
     public void setShouldRestart(boolean shouldRestart) {
         this.shouldRestart = shouldRestart;
     }
 
+    /**
+     * @param idx Index ti retrieve
+     * @return Command from history
+     */
     public String getCommandFromHistory(int idx) {
         return commandHistory.get(idx);
     }
 
+    /**
+     * @return Number of commands in the history
+     */
     public int getHistorySize() {
         return commandHistory.size();
     }
 
+    /**
+     * @return Server port
+     */
     public int getPort() {
         return port;
     }
