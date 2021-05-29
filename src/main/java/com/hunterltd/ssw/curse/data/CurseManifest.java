66849a71c09b6c2c09262772d47c443414430116
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-05-29 17:30:55 -0400

    CurseManifest.java

--- src/main/java/com/hunterltd/ssw/curse/data/CurseManifest.java
+++ src/main/java/com/hunterltd/ssw/curse/data/CurseManifest.java
@@ -8,21 +8,40 @@ import org.json.simple.parser.ParseException;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * A CurseForge manifest wrapper class
+ */
 public class CurseManifest extends JSONObject {
     private final File manifestFile;
 
+    /**
+     * Instantiates the wrapper class from the manifest file object
+     * @param manifestFile Manifest file
+     */
     public CurseManifest(File manifestFile) {
         this.manifestFile = manifestFile;
     }
 
+    /**
+     * Loads the manifest.json data
+     * @throws IOException if an I/O error occurs reading the dada
+     * @throws ParseException if a parsing error occurs reading the data
+     */
     public void load() throws IOException, ParseException {
         UserDataObject.readData(manifestFile, this);
     }
 
+    /**
+     * @return The manifest file object
+     */
     public File getFile() {
         return manifestFile;
     }
 
+    /**
+     * Creates {@link CurseManifestFileEntry} objects from the files array in the manifest
+     * @return Array of ManifestEntry objects from the files array in the manifest
+     */
     public CurseManifestFileEntry[] getFiles() {
         JSONArray jsonArr = (JSONArray) this.get("files");
         CurseManifestFileEntry[] addons = new CurseManifestFileEntry[jsonArr.size()];
