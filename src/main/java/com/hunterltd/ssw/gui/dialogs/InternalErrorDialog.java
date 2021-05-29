1763290aa3bc49355b0e6e3de1fb01a6af2aac3c
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-05-28 20:39:57 -0400

    Made title the exception name

--- src/main/java/com/hunterltd/ssw/gui/dialogs/InternalErrorDialog.java
+++ src/main/java/com/hunterltd/ssw/gui/dialogs/InternalErrorDialog.java
@@ -2,7 +2,7 @@ package com.hunterltd.ssw.gui.dialogs;
 
 public class InternalErrorDialog extends InfoDialog {
     public InternalErrorDialog(Exception exception) {
-        super("Internal Error", "An internal error occurred: " + exception.getLocalizedMessage());
+        super(exception.getClass().getSimpleName(), "An internal error occurred: " + exception.getLocalizedMessage());
         this.pack();
         this.setVisible(true);
     }
