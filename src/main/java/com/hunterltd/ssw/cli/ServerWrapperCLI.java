af26fc8d69a2b9bf43c75a789de6531245536d80
dablenparty <hdavenport8902@gmail.com>
2021-06-01 22:57:20 -0400

    Added dynamic showVersion method

--- src/main/java/com/hunterltd/ssw/cli/ServerWrapperCLI.java
+++ src/main/java/com/hunterltd/ssw/cli/ServerWrapperCLI.java
@@ -1,7 +1,21 @@
 package com.hunterltd.ssw.cli;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ServerWrapperCLI {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello from the CLI");
+        showVersion();
+    }
+
+    public static void showVersion() throws IOException {
+        Properties mavenProperties = new Properties();
+        try (InputStream resourceStream =
+                     ServerWrapperCLI.class.getClassLoader().getResourceAsStream("project.properties")) {
+            mavenProperties.load(resourceStream);
+        }
+        System.out.println(mavenProperties.getProperty("artifactId") + " v" + mavenProperties.getProperty("version"));
     }
 }
