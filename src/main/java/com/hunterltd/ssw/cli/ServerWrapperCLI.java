dc65300bf4fbe4ddb8246cfbf00e95e52a90352b
dablenparty <hdavenport8902@gmail.com>
2021-06-01 23:29:20 -0400

    Added constructor and showVersion method

--- src/main/java/com/hunterltd/ssw/cli/ServerWrapperCLI.java
+++ src/main/java/com/hunterltd/ssw/cli/ServerWrapperCLI.java
@@ -2,20 +2,39 @@ package com.hunterltd.ssw.cli;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 
 public class ServerWrapperCLI {
-    public static void main(String[] args) throws IOException {
+    private final Properties mavenProperties;
+    private final boolean propertiesLoaded;
+
+    public static void main(String[] args) {
         System.out.println("Hello from the CLI");
-        showVersion();
+        ServerWrapperCLI wrapperCli = new ServerWrapperCLI();
+        wrapperCli.showVersion();
     }
 
-    public static void showVersion() throws IOException {
-        Properties mavenProperties = new Properties();
+    ServerWrapperCLI() {
+        mavenProperties = new Properties();
+        propertiesLoaded = loadProperties();
+    }
+
+    public void showVersion() {
+        if (propertiesLoaded)
+            System.out.println(mavenProperties.getProperty("artifactId") + " v" + mavenProperties.getProperty("version")
+                    + " on " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("MMM dd yyyy, HH:mm:ss")));
+    }
+
+    private boolean loadProperties() {
         try (InputStream resourceStream =
                      ServerWrapperCLI.class.getClassLoader().getResourceAsStream("project.properties")) {
             mavenProperties.load(resourceStream);
+            return true;
+        } catch (IOException exception) {
+            System.err.println(exception.getLocalizedMessage());
+            return false;
         }
-        System.out.println(mavenProperties.getProperty("artifactId") + " v" + mavenProperties.getProperty("version"));
     }
 }
