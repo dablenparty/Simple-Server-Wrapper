62095be5aeb4f00506b131b690d11a3a963fdea8
dablenparty <hdavenport8902@gmail.com>
2021-06-03 02:27:38 -0400

    Merge branch 'cli-threading'

---
+++ src/main/java/com/hunterltd/ssw/cli/ServerShutdownTask.java
@@ -0,0 +1,18 @@
+package com.hunterltd.ssw.cli;
+
+import com.hunterltd.ssw.server.MinecraftServer;
+
+public class ServerShutdownTask implements Runnable {
+    private final MinecraftServer server;
+
+    public ServerShutdownTask(MinecraftServer server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Nobody has joined in a while, closing the server...");
+        server.setShouldBeRunning(false);
+        server.setShuttingDown(true);
+    }
+}
--- src/main/java/com/hunterltd/ssw/cli/ServerWrapperCLI.java
+++ src/main/java/com/hunterltd/ssw/cli/ServerWrapperCLI.java
@@ -1,6 +1,7 @@
 package com.hunterltd.ssw.cli;
 
 import com.hunterltd.ssw.server.MinecraftServer;
+import com.hunterltd.ssw.server.StreamGobbler;
 import com.hunterltd.ssw.utilities.Settings;
 
 import java.io.File;
@@ -9,20 +10,95 @@ import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ServerWrapperCLI {
     private final Properties mavenProperties;
     private final boolean propertiesLoaded;
-    private volatile MinecraftServer minecraftServer;
+    private final MinecraftServer minecraftServer;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             // TODO: write help message
             throw new IllegalArgumentException((args.length > 1 ? "Too many" : "Missing") + " arguments");
         }
 
         ServerWrapperCLI wrapperCli = new ServerWrapperCLI(new File(args[0]));
+        MinecraftServer minecraftServer = wrapperCli.getMinecraftServer();
+        ExecutorService inputService = null, errorService = null;
+        ScheduledExecutorService serverStateService = Executors.newScheduledThreadPool(1),
+                serverPingService = null;
+        ServerPingTask pingTask = null;
+        if (minecraftServer.getServerSettings().getShutdown()) {
+            serverPingService = Executors.newScheduledThreadPool(1);
+            pingTask = new ServerPingTask(minecraftServer);
+        }
+        serverStateService.scheduleWithFixedDelay(new AliveStateCheckTask(minecraftServer), 100L, 100L, TimeUnit.MILLISECONDS);
         wrapperCli.showVersion();
+
+        doLoop: do {
+            Scanner inputScanner = new Scanner(System.in);
+            String command = inputScanner.nextLine();
+            switch (command) {
+                case "start":
+                    System.out.println("Starting server...");
+                    minecraftServer.setShouldBeRunning(true);
+                    minecraftServer.run();
+                    // submits process streams to stream gobblers to redirect output to standard out and error
+                    inputService = StreamGobbler.execute(minecraftServer.getServerProcess().getInputStream(), System.out::println);
+                    errorService = StreamGobbler.execute(minecraftServer.getServerProcess().getErrorStream(), System.err::println);
+                    if (serverPingService != null) {
+                        serverPingService.scheduleWithFixedDelay(pingTask, 2L, 2L, TimeUnit.SECONDS);
+                    }
+                    break;
+                case "stop":
+                    if (minecraftServer.isRunning()) {
+                        minecraftServer.setShouldBeRunning(false);
+                    }
+
+                    if (inputService != null) {
+                        tryShutdownExecutorService(inputService);
+                        tryShutdownExecutorService(errorService);
+                    }
+                    break;
+                case "close":
+                    if (minecraftServer.isRunning()) {
+                        minecraftServer.setShouldBeRunning(false);
+                    }
+                    for (ExecutorService service :
+                            new ExecutorService[]{inputService, errorService, serverPingService, serverStateService}) {
+                        if (service != null)
+                            tryShutdownExecutorService(service);
+                    }
+                    if (pingTask != null && pingTask.getShutdownService() != null)
+                        tryShutdownExecutorService(pingTask.getShutdownService());
+                    inputScanner.close();
+                    break doLoop;
+                default:
+                    if (minecraftServer.isRunning()) minecraftServer.sendCommand(command.strip());
+                    break;
+            }
+        } while (true);
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void tryShutdownExecutorService(ExecutorService service) {
+        System.out.println("Shutting down background service...");
+        service.shutdown();
+        try {
+            service.awaitTermination(5L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.err.println(e.getLocalizedMessage());
+        } finally {
+            if (!service.isTerminated())
+                System.err.println("Service didn't terminate, forcing shutdown");
+            service.shutdownNow();
+            System.out.println("Service closed");
+        }
     }
 
     ServerWrapperCLI(File serverFile) {
@@ -46,5 +122,9 @@ public class ServerWrapperCLI {
             System.err.println(exception.getLocalizedMessage());
             return false;
         }
+    }
+
+    public MinecraftServer getMinecraftServer() {
+        return minecraftServer;
     }
 }
