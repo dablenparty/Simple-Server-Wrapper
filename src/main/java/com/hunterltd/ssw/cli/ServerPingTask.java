6a369dd583ec9d28bc27b619f6d42dcf9ba5b577
dablenparty <hdavenport8902@gmail.com>
2021-06-02 23:47:22 -0400

    Implemented proof of concept shutdown timer

--- src/main/java/com/hunterltd/ssw/cli/ServerPingTask.java
+++ src/main/java/com/hunterltd/ssw/cli/ServerPingTask.java
@@ -5,10 +5,14 @@ import com.hunterltd.ssw.utilities.ServerListPing;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ServerPingTask implements Runnable {
     private final MinecraftServer server;
     private final ServerListPing pinger = new ServerListPing();
+    private volatile ScheduledExecutorService shutdownService = null;
 
     public ServerPingTask(MinecraftServer server) {
         this.server = server;
@@ -22,12 +26,20 @@ public class ServerPingTask implements Runnable {
         try {
             ServerListPing.StatusResponse response = pinger.fetchData();
             if (server.isRunning() && !server.isShuttingDown()) {
-                if (response.getPlayers().getOnline() == 0) {
+                if (response.getPlayers().getOnline() == 0 && shutdownService == null) {
                     // shutdown timer begins
+                    shutdownService = Executors.newScheduledThreadPool(1);
+                    long delay = server.getServerSettings().getShutdownInterval();
+                    shutdownService.schedule(new ServerShutdownTask(server), delay, TimeUnit.MINUTES);
+                }
+            } else {
+                if (shutdownService != null) {
+                    ServerWrapperCLI.tryShutdownExecutorService(shutdownService);
+                    shutdownService = null;
                 }
             }
-        } catch (IOException exception) {
-            exception.printStackTrace();
+        } catch (IOException | NullPointerException exception) {
+//            exception.printStackTrace();
         }
     }
 }
