35a133d1dc2d14c15645ec81d72d0a9e1c290eca
dablenparty <hdavenport8902@gmail.com>
2021-06-03 16:18:03 -0400

    Added downloading logic

--- src/main/java/com/hunterltd/ssw/cli/CurseCli.java
+++ src/main/java/com/hunterltd/ssw/cli/CurseCli.java
@@ -1,11 +1,21 @@
 package com.hunterltd.ssw.cli;
 
+import com.hunterltd.ssw.curse.CurseAddon;
 import com.hunterltd.ssw.curse.CurseModpack;
+import com.hunterltd.ssw.curse.data.CurseManifestFileEntry;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.apache.commons.io.FileUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CurseCli {
@@ -23,6 +33,39 @@ public class CurseCli {
             extractModpack(scanner);
         } catch (IOException | ParseException exception) {
             exception.printStackTrace();
+        }
+
+        System.out.println("Reading manifest...");
+        CurseManifestFileEntry[] files = curseModpack.getManifest().getFiles();
+        File modsFolder = Paths.get(serverFolder.toString(), "mods").toFile();
+        try {
+            if (Objects.requireNonNull(modsFolder.listFiles()).length != 0)
+                FileUtils.deleteDirectory(modsFolder);
+            else return;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        } catch (NullPointerException ignored) {}
+
+        Client client = ClientBuilder.newClient();
+        int filesLength = files.length;
+
+        for (int i = 0; i < filesLength; i++) {
+            CurseManifestFileEntry manifestFileEntry = files[i];
+            String target = String.format("https://addons-ecs.forgesvc.net/api/v2/addon/%d/file/%d",
+                    manifestFileEntry.getProjectID(),
+                    manifestFileEntry.getFileID());
+            Response response = client.target(target).request(MediaType.APPLICATION_JSON).get();
+            try {
+                JSONObject responseData = (JSONObject) new JSONParser().parse(response.readEntity(String.class));
+                CurseAddon addon = new CurseAddon(responseData);
+                System.out.printf("Mod %d of %d: %s%n", i + 1, filesLength, addon);
+                addon.download(serverFolder.toString());
+            } catch (ParseException | IOException e) {
+                String errorMessage = e instanceof ParseException ?
+                        "Failed to parse JSON data for an addon" : "Failed to download an addon";
+                System.err.println(errorMessage);
+            }
         }
     }
 
