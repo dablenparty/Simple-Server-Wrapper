f04119f00b136561d5f19c200603035bd81cf4b2
dablenparty <hdavenport8902@gmail.com>
2021-06-02 20:10:24 -0400

    Implemented constructor and overridden run method

--- src/main/java/com/hunterltd/ssw/cli/ServerPingTask.java
+++ src/main/java/com/hunterltd/ssw/cli/ServerPingTask.java
@@ -1,8 +1,33 @@
 package com.hunterltd.ssw.cli;
 
+import com.hunterltd.ssw.server.MinecraftServer;
+import com.hunterltd.ssw.utilities.ServerListPing;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class ServerPingTask implements Runnable {
+    private final MinecraftServer server;
+    private final ServerListPing pinger = new ServerListPing();
+
+    public ServerPingTask(MinecraftServer server) {
+        this.server = server;
+        pinger.setAddress(new InetSocketAddress(server.getPort()));
+    }
+
     @Override
     public void run() {
+        if (!server.isRunning() || server.isShuttingDown()) return;
 
+        try {
+            ServerListPing.StatusResponse response = pinger.fetchData();
+            if (server.isRunning() && !server.isShuttingDown()) {
+                if (response.getPlayers().getOnline() == 0) {
+                    // shutdown timer begins
+                }
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
