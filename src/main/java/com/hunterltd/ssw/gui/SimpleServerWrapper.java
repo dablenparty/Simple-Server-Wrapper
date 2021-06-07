c480a92d4c92d71243acd468154c07fb4bbcca3e
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-06-07 13:46:03 -0400

    Cleaned up SimpleServerWrapper.java

--- src/main/java/com/hunterltd/ssw/gui/SimpleServerWrapper.java
+++ src/main/java/com/hunterltd/ssw/gui/SimpleServerWrapper.java
@@ -14,37 +14,6 @@ public class SimpleServerWrapper {
         }
         SwingUtilities.invokeLater(() -> {
             WrapperGUI wrapperGUI = new WrapperGUI();
-            // If server is running, it needs to be shut down before the wrapper closes
-            wrapperGUI.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-            wrapperGUI.addWindowListener(new WindowAdapter() {
-                @Override
-                public void windowClosing(WindowEvent e) {
-                    JFrame frame = (JFrame) e.getSource();
-
-                    if (wrapperGUI.getServer() != null &&
-                            wrapperGUI.getServer().isRunning())
-                    {
-                        int result = JOptionPane.showConfirmDialog(
-                                frame,
-                                "Are you sure you want to exit the server wrapper? This will stop the server.",
-                                "Exit Server Wrapper",
-                                JOptionPane.YES_NO_OPTION
-                        );
-                        if (result == JOptionPane.YES_OPTION) {
-                            try {
-                                wrapperGUI.getServer().stop();
-                            } catch (IOException | InterruptedException ioException) {
-                                ioException.printStackTrace();
-                                wrapperGUI.getServer().getServerProcess().destroy(); // Failsafe
-                            }
-                            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        }
-                    } else {
-                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    }
-
-                }
-            });
             wrapperGUI.pack();
             wrapperGUI.setVisible(true);
         });
