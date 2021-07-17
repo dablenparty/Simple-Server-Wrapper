af1cf94d094a777211bd65ddc756f365c4d57221
dablenparty <hdavenport8902@gmail.com>
2021-07-16 22:43:34 -0400

    Added basic start, stop, parseArgs, and main methods

--- src/main/java/com/hunterltd/ssw/cli/SswServerCli.java
+++ src/main/java/com/hunterltd/ssw/cli/SswServerCli.java
@@ -1,4 +1,79 @@
 package com.hunterltd.ssw.cli;
 
+import com.hunterltd.ssw.utilities.MavenUtils;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+
 public class SswServerCli {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private final int port;
+    private final File serverFile;
+
+    public static Namespace parseArgs(String[] args) {
+        Properties mavenProperties = MavenUtils.getMavenProperties();
+        ArgumentParser parser = ArgumentParsers.newFor("Simple Server Wrapper Server CLI").build()
+                .defaultHelp(true)
+                .version(String.format("${prog} v%s", mavenProperties.getProperty("version")))
+                .description("Server-side commandline interface for running and interacting with a Minecraft server." +
+                        " This also allows for remote or local connections from the client-side counterpart to this program");
+        parser.addArgument("-v", "--version")
+                .action(Arguments.version());
+
+        parser.addArgument("server")
+                .action(Arguments.store())
+                .metavar("SERVER_PATH")
+                .help("path to server jar file");
+
+        parser.addArgument("-m", "--modpack")
+                .action(Arguments.store())
+                .required(false)
+                .help("CurseForge modpack zip file to install to server");
+
+        parser.addArgument("-p", "--port")
+                .action(Arguments.store())
+                .setDefault(9609)
+                .help("port number to connect on");
+
+        return parser.parseArgsOrFail(args);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Namespace namespace = parseArgs(args);
+        File server = new File(namespace.getString("server")).getAbsoluteFile();
+        int port = namespace.getInt("port");
+        SswServerCli serverCli = new SswServerCli(port, server);
+        serverCli.start();
+    }
+
+    SswServerCli(int port, File serverFile) {
+        this.port = port;
+        this.serverFile = serverFile;
+    }
+
+    public void start() throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String message = in.readLine();
+        System.out.printf("Message from client: %s", message);
+        out.println("hello from the server!");
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
 }
