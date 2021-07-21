5f336b036357b731062545ec11bd26acedfd4f36
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-07-21 09:43:23 -0400

    Added proof of concept login step

--- src/main/java/com/hunterltd/ssw/cli/SswServerCli.java
+++ src/main/java/com/hunterltd/ssw/cli/SswServerCli.java
@@ -1,6 +1,7 @@
 package com.hunterltd.ssw.cli;
 
 import com.hunterltd.ssw.utilities.MavenUtils;
+import com.hunterltd.ssw.utilities.PasswordHasher;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
@@ -9,6 +10,7 @@ import net.sourceforge.argparse4j.inf.Namespace;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.Properties;
 
 public class SswServerCli {
@@ -66,8 +68,23 @@ public class SswServerCli {
         clientSocket = serverSocket.accept();
         System.out.printf("Connection accepted from %s on port %s%n",
                 clientSocket.getInetAddress(), clientSocket.getPort());
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        // scopes password handling so that the garbage collector deletes it from memory after
+        {
+            out.print("username: ");
+            String username = in.readLine();
+            out.print("password: ");
+            String passwordHash = new String(Objects.requireNonNull(PasswordHasher.hashPassword(in.readLine())));
+            String fromFile = ""; // replace with file read operation from wrapperSettings.json
+            if (!(passwordHash.equals(fromFile) && username.equals(fromFile))) {
+                out.println("Wrong credentials");
+                stop();
+                return;
+            }
+        }
+        out.println("Successfully logged in.");
+
         String message;
         while ((message = in.readLine()) != null) {
             System.out.printf("[Client %s:%s] %s%n",
