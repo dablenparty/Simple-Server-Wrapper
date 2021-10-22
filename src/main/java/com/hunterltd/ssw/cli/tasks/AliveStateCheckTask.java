0bbb9de656815e0a4e12695d8c290588a6d0303d
dablenparty <hdavenport8902@gmail.com>
2021-10-22 14:35:21 -0400

    Implemented the PortListener

--- src/main/java/com/hunterltd/ssw/cli/tasks/AliveStateCheckTask.java
+++ src/main/java/com/hunterltd/ssw/cli/tasks/AliveStateCheckTask.java
@@ -1,12 +1,17 @@
 package com.hunterltd.ssw.cli.tasks;
 
 import com.hunterltd.ssw.server.MinecraftServer;
+import com.hunterltd.ssw.utilities.ThreadUtils;
+import com.hunterltd.ssw.utilities.network.PortListener;
 
 import java.io.IOException;
 
 public class AliveStateCheckTask extends ServerBasedRunnable {
+    private final PortListener portListener;
+
     public AliveStateCheckTask(MinecraftServer minecraftServer) {
         super(minecraftServer);
+        portListener = new PortListener(minecraftServer.getPort());
     }
 
     @Override
@@ -14,8 +19,10 @@ public class AliveStateCheckTask extends ServerBasedRunnable {
         MinecraftServer server = getMinecraftServer();
         if (!server.shouldBeRunning() && server.isRunning() && !server.isShuttingDown()) {
             server.stop();
-            // this is where the PortListener would start, and this thread would be its owner rather than main
+            startPortListener(server);
         } else if (server.shouldBeRunning() && !server.isRunning()) {
+            if (portListener.isOpen())
+                portListener.stop();
             try {
                 server.run();
             } catch (IOException e) {
@@ -23,6 +30,23 @@ public class AliveStateCheckTask extends ServerBasedRunnable {
                 e.printStackTrace();
                 server.setShouldBeRunning(false);
             }
+        }
+    }
+
+    private void startPortListener(MinecraftServer server) {
+        if (!server.getServerSettings().getShutdown() || portListener.isOpen()) return;
+        System.out.println(ThreadUtils.threadStampString(String.format("Attempting to open port listener on port %d", server.getPort())));
+        try {
+            portListener.start();
+            portListener.on("connection", objects -> server.setShouldBeRunning(true));
+//            portListener.on("close", objects -> portListener.stop());
+            portListener.on("error", errors -> {
+                for (Object e : errors)
+                    ((Exception) e).printStackTrace();
+            });
+            portListener.on("stop", objects -> System.out.println(ThreadUtils.threadStampString("Port listener closed")));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
