5fec34fb252212db9402268ed810923a00ea94f6
dablenparty <hdavenport8902@gmail.com>
2021-10-22 12:16:56 -0400

    First implementation of ServerPingTask.run

--- src/main/java/com/hunterltd/ssw/cli/tasks/ServerPingTask.java
+++ src/main/java/com/hunterltd/ssw/cli/tasks/ServerPingTask.java
@@ -1,9 +1,12 @@
 package com.hunterltd.ssw.cli.tasks;
 
+import com.hunterltd.ssw.cli.SswServerCli;
 import com.hunterltd.ssw.server.MinecraftServer;
 import com.hunterltd.ssw.utilities.NamedExecutorService;
+import com.hunterltd.ssw.utilities.ThreadUtils;
 import com.hunterltd.ssw.utilities.network.ServerListPing;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -12,7 +15,8 @@ import java.util.concurrent.TimeUnit;
 
 public class ServerPingTask extends ServerBasedRunnable {
     private final ServerListPing pinger = new ServerListPing();
-    private volatile NamedExecutorService scheduledShutdownService = null;
+    private final NamedExecutorService scheduledShutdownService;
+    private ScheduledFuture<?> shutdownServiceFuture = null;
 
     protected ServerPingTask(MinecraftServer minecraftServer) {
         super(minecraftServer);
@@ -24,6 +28,28 @@ public class ServerPingTask extends ServerBasedRunnable {
 
     @Override
     public void run() {
-
+        MinecraftServer minecraftServer = getMinecraftServer();
+        if (minecraftServer.shouldBeRunning() && minecraftServer.isRunning() && !minecraftServer.isShuttingDown()) {
+            ServerListPing.StatusResponse response = null;
+            try {
+                response = pinger.fetchData();
+            } catch (IOException e) {
+                SswServerCli.printExceptionToOut(e);
+            }
+            if (response == null) return;
+            int onlinePlayers = response.getPlayers().getOnline();
+            ScheduledExecutorService executorService = (ScheduledExecutorService) scheduledShutdownService.executorService();
+            if (onlinePlayers == 0) {
+                if (shutdownServiceFuture == null || shutdownServiceFuture.isDone()) {
+                    // shutdown timer begins
+                    long delay = minecraftServer.getServerSettings().getShutdownInterval();
+                    shutdownServiceFuture = executorService.schedule(() -> minecraftServer.setShouldBeRunning(false), delay, TimeUnit.MINUTES);
+                }
+            } else if (shutdownServiceFuture != null && executorService != null) {
+                boolean didShutdown = shutdownServiceFuture.cancel(false);
+                if (!didShutdown && !shutdownServiceFuture.isDone())
+                    shutdownServiceFuture.cancel(true);
+            }
+        }
     }
 }
