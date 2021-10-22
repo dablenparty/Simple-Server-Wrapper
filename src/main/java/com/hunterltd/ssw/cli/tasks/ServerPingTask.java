45f2497a07ef6a5af905431a132a9a3181c25971
dablenparty <hdavenport8902@gmail.com>
2021-10-22 11:49:24 -0400

    Put creation of thread pool in constructor

--- src/main/java/com/hunterltd/ssw/cli/tasks/ServerPingTask.java
+++ src/main/java/com/hunterltd/ssw/cli/tasks/ServerPingTask.java
@@ -5,7 +5,10 @@ import com.hunterltd.ssw.utilities.NamedExecutorService;
 import com.hunterltd.ssw.utilities.network.ServerListPing;
 
 import java.net.InetSocketAddress;
-import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class ServerPingTask extends ServerBasedRunnable {
     private final ServerListPing pinger = new ServerListPing();
@@ -14,6 +17,9 @@ public class ServerPingTask extends ServerBasedRunnable {
     protected ServerPingTask(MinecraftServer minecraftServer) {
         super(minecraftServer);
         pinger.setAddress(new InetSocketAddress(minecraftServer.getPort()));
+        String serviceName = "MinecraftServer Shutdown Service";
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(1, ThreadUtils.newNamedThreadFactory(serviceName));
+        scheduledShutdownService = new NamedExecutorService(serviceName, service);
     }
 
     @Override
