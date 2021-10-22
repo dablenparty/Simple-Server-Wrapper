0114d3b12742c1b403bfc54ce8cc3c33ff230fae
dablenparty <hdavenport8902@gmail.com>
2021-10-22 13:36:30 -0400

    Converted NamedExecutorService from a record to a class

--- src/main/java/com/hunterltd/ssw/utilities/NamedExecutorService.java
+++ src/main/java/com/hunterltd/ssw/utilities/NamedExecutorService.java
@@ -1,9 +1,49 @@
 package com.hunterltd.ssw.utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 
 /**
  * An ExecutorService with a {@code name} string
  */
-public record NamedExecutorService(String name, ExecutorService executorService) {
+public final class NamedExecutorService {
+    private final String name;
+    private final ExecutorService executorService;
+    private final List<NamedExecutorService> childServices = new ArrayList<>(1);
+
+    public NamedExecutorService(String name, ExecutorService executorService) {
+        this.name = name;
+        this.executorService = executorService;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public ExecutorService service() {
+        return executorService;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (NamedExecutorService) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.executorService, that.executorService);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, executorService);
+    }
+
+    @Override
+    public String toString() {
+        return "NamedExecutorService[" +
+                "name=" + name + ", " +
+                "executorService=" + executorService + ']';
+    }
 }
