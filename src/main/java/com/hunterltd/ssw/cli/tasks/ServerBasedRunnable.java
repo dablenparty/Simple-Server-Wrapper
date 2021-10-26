3121af1fafacd3547e5001c2bb93378e713cc7d2
dablenparty <hdavenport8902@gmail.com>
2021-10-26 13:39:42 -0400

    Created methods for interacting with childServices list

--- src/main/java/com/hunterltd/ssw/cli/tasks/ServerBasedRunnable.java
+++ src/main/java/com/hunterltd/ssw/cli/tasks/ServerBasedRunnable.java
@@ -1,9 +1,15 @@
 package com.hunterltd.ssw.cli.tasks;
 
 import com.hunterltd.ssw.server.MinecraftServer;
+import com.hunterltd.ssw.utilities.concurrency.NamedExecutorService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public abstract class ServerBasedRunnable implements Runnable {
     private final MinecraftServer minecraftServer;
+    private final List<NamedExecutorService> childServices = new ArrayList<>(1);
 
     protected ServerBasedRunnable(MinecraftServer minecraftServer) {
         this.minecraftServer = minecraftServer;
@@ -11,5 +17,26 @@ public abstract class ServerBasedRunnable implements Runnable {
 
     public MinecraftServer getMinecraftServer() {
         return minecraftServer;
+    }
+
+    protected final void addChildService(NamedExecutorService service) {
+        childServices.add(service);
+    }
+
+    protected final void removeChildService(NamedExecutorService service) {
+        childServices.remove(service);
+    }
+
+    protected final NamedExecutorService removeChildService(int index) {
+        return childServices.remove(index);
+    }
+
+    /**
+     * Gets list of child services as an unmodifiable list
+     *
+     * @return List of services spawned by this task
+     */
+    public final List<NamedExecutorService> getChildServices() {
+        return Collections.unmodifiableList(childServices);
     }
 }
