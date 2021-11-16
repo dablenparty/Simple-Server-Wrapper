4ae0c6e5678e868901ff78eea98948943768ae9f
dablenparty <hdavenport8902@gmail.com>
2021-11-16 00:39:49 -0500

    Deleted old GUI code

--- src/main/java/com/hunterltd/ssw/gui/dialogs/InternalErrorDialog.java
+++
@@ -1,9 +0,0 @@
-package com.hunterltd.ssw.gui.dialogs;
-
-public class InternalErrorDialog extends InfoDialog {
-    public InternalErrorDialog(Exception exception) {
-        super(exception.getClass().getSimpleName(), "An internal error occurred: " + exception.getLocalizedMessage());
-        this.pack();
-        this.setVisible(true);
-    }
-}
