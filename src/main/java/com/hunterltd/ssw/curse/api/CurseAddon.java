8b11eed99174bee9496208d375aa029b4255cc5a
dablenparty <hdavenport8902@gmail.com>
2021-11-16 16:28:05 -0500

    Added getters and factory method for reading JSON

--- src/main/java/com/hunterltd/ssw/curse/api/CurseAddon.java
+++ src/main/java/com/hunterltd/ssw/curse/api/CurseAddon.java
@@ -1,5 +1,6 @@
 package com.hunterltd.ssw.curse.api;
 
+import com.google.gson.Gson;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -13,13 +14,29 @@ public class CurseAddon {
     private URL downloadUrl;
     private String fileName;
 
+    public static CurseAddon newCurseAddon(String jsonData) {
+        return new Gson().fromJson(jsonData, CurseAddon.class);
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public URL getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
     @Override
     public String toString() {
         return displayName;
     }
 
     public void download(String destinationPath) throws IOException {
-        String folderName = fileName.endsWith(".zip") ? "resourcepacks" :"mods";
+        String folderName = fileName.endsWith(".zip") ? "resourcepacks" : "mods";
         File destinationFile = Paths.get(destinationPath, folderName, fileName).toFile();
         FileUtils.copyURLToFile(downloadUrl, destinationFile, CONNECTION_TIMEOUT, CONNECTION_TIMEOUT);
     }
