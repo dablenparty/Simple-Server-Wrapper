5337ef56b94019239bd3dd45db6d692c31c506ce
dablenparty <hdavenport8902@gmail.com>
2021-12-17 18:01:37 -0500

    Now opens a settings window when the Settings menu is clicked

--- src/main/java/com/hunterltd/ssw/gui/controllers/SimpleServerWrapperController.java
+++ src/main/java/com/hunterltd/ssw/gui/controllers/SimpleServerWrapperController.java
@@ -1,22 +1,33 @@
 package com.hunterltd.ssw.gui.controllers;
 
+import com.hunterltd.ssw.gui.SimpleServerWrapperGui;
 import com.hunterltd.ssw.minecraft.MinecraftServer;
 import com.hunterltd.ssw.util.concurrency.NamedExecutorService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.*;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static com.hunterltd.ssw.util.concurrency.ThreadUtils.runOnFxThread;
@@ -115,6 +126,18 @@ public class SimpleServerWrapperController {
         serverPathTextField.setText(chosen.toString());
         runButton.setDisable(false);
         serverMenu.setDisable(false);
+    }
+
+    @FXML
+    protected void onSettingsMenuClick(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        URL viewResource = SimpleServerWrapperGui.class.getResource("server-settings-view.fxml");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(viewResource));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Server Settings");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(((MenuItem)event.getSource()).getStyleableNode().getScene().getWindow());
+        stage.show();
     }
 
     @FXML
