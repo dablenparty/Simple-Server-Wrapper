f69e0e68095acae15f2af990086fb276626bcbfe
dablenparty <hdavenport8902@gmail.com>
2021-12-17 20:02:06 -0500

    Added outputtedText property

--- src/main/java/com/hunterltd/ssw/gui/model/SimpleServerWrapperModel.java
+++ src/main/java/com/hunterltd/ssw/gui/model/SimpleServerWrapperModel.java
@@ -1,4 +1,28 @@
 package com.hunterltd.ssw.gui.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class SimpleServerWrapperModel {
+    private final StringProperty outputtedText;
+
+    public SimpleServerWrapperModel() {
+        outputtedText = new SimpleStringProperty();
+    }
+
+    public String getOutputtedText() {
+        return outputtedText.get();
+    }
+
+    public StringProperty outputtedTextProperty() {
+        return outputtedText;
+    }
+
+    public void setOutputtedText(String outputtedText) {
+        this.outputtedText.set(outputtedText);
+    }
+
+    public void appendToOutputtedText(String text) {
+        this.outputtedText.set(getOutputtedText() + text);
+    }
 }
