fdb4e45b7e6a7e2843b46e53af6fbf1f28643e28
dablenparty <hdavenport8902@gmail.com>
2021-12-17 20:11:19 -0500

    Created added TextArea property

--- src/main/java/com/hunterltd/ssw/util/gui/SmartScrollerFx.java
+++ src/main/java/com/hunterltd/ssw/util/gui/SmartScrollerFx.java
@@ -1,4 +1,11 @@
 package com.hunterltd.ssw.util.gui;
 
+import javafx.scene.control.TextArea;
+
 public class SmartScrollerFx {
+    private final TextArea textArea;
+
+    public SmartScrollerFx(TextArea textArea) {
+        this.textArea = textArea;
+    }
 }
