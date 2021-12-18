6f08190a06187b6aacc36df9a915892c4164c761
dablenparty <hdavenport8902@gmail.com>
2021-12-17 20:11:48 -0500

    Renamed SmartScrollerFx.java to SmartScrollerFxTextArea.java

--- src/main/java/com/hunterltd/ssw/util/gui/SmartScrollerFxTextArea.java
+++ src/main/java/com/hunterltd/ssw/util/gui/SmartScrollerFxTextArea.java
@@ -2,10 +2,10 @@ package com.hunterltd.ssw.util.gui;
 
 import javafx.scene.control.TextArea;
 
-public class SmartScrollerFx {
+public class SmartScrollerFxTextArea {
     private final TextArea textArea;
 
-    public SmartScrollerFx(TextArea textArea) {
+    public SmartScrollerFxTextArea(TextArea textArea) {
         this.textArea = textArea;
     }
 }
