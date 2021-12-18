43ec9ce01c7d831ff254683f36d913807d415c2f
dablenparty <hdavenport8902@gmail.com>
2021-12-17 20:44:36 -0500

    Barbaric SmartScroller implementation

--- src/main/java/com/hunterltd/ssw/util/gui/SmartScrollerFxTextArea.java
+++ src/main/java/com/hunterltd/ssw/util/gui/SmartScrollerFxTextArea.java
@@ -1,11 +1,41 @@
 package com.hunterltd.ssw.util.gui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextArea;
 
-public class SmartScrollerFxTextArea {
+public class SmartScrollerFxTextArea implements ChangeListener<String> {
     private final TextArea textArea;
+    private boolean hasScrolled = false;
+    private double previousScrollValue = -1;
+    private double previousMaximumScrollValue = -1;
 
     public SmartScrollerFxTextArea(TextArea textArea) {
         this.textArea = textArea;
+    }
+
+    @Override
+    public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+        double scrollTop = textArea.getScrollTop();
+        System.out.println(scrollTop);
+        double maximum = calculateMaximumScroll(scrollTop);
+        if (scrollTop == previousMaximumScrollValue) {
+            previousMaximumScrollValue = maximum;
+            previousScrollValue = scrollTop;
+            textArea.selectPositionCaret(textArea.getLength());
+            textArea.deselect();
+            // auto scroll
+        } else {
+            // do nothing
+        }
+    }
+
+    private double calculateMaximumScroll(double oldPosition) {
+        // scroll to bottom
+        textArea.selectPositionCaret(textArea.getLength());
+        textArea.deselect();
+        double maximum = textArea.getScrollTop();
+        textArea.setScrollTop(oldPosition);
+        return maximum;
     }
 }
