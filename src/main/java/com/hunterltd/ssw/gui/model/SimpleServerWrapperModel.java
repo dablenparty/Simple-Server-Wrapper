15332ede86cf66b33ecef2d2376fd4bde016c10a
dablenparty <hdavenport8902@gmail.com>
2021-12-27 13:10:23 -0500

    Added new dynamically calculated server memory property to model

--- src/main/java/com/hunterltd/ssw/gui/model/SimpleServerWrapperModel.java
+++ src/main/java/com/hunterltd/ssw/gui/model/SimpleServerWrapperModel.java
@@ -1,25 +1,49 @@
 package com.hunterltd.ssw.gui.model;
 
+import com.sun.management.OperatingSystemMXBean;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 public class SimpleServerWrapperModel {
     private final StringProperty outputtedText;
+    private final ObservableList<Double> serverMemory;
 
     public SimpleServerWrapperModel() {
         outputtedText = new SimpleStringProperty();
+        double memGigabytes = calculateSystemMemoryGb();
+        ArrayList<Double> memOpts = DoubleStream
+                .iterate(0.5, i -> i < memGigabytes, i -> i + 0.5)
+                .boxed()
+                .collect(Collectors.toCollection(() -> new ArrayList<>((int) Math.round(memGigabytes) * 2)));
+        serverMemory = FXCollections.observableArrayList(memOpts);
+    }
+
+    private double calculateSystemMemoryGb() {
+        long memBytes = ((OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getTotalMemorySize();
+        return ((double) memBytes) / Math.pow(1024, 3);
+    }
+
+    public ObservableList<Double> getServerMemory() {
+        return serverMemory;
     }
 
     public String getOutputtedText() {
         return outputtedText.get();
     }
 
-    public StringProperty outputtedTextProperty() {
-        return outputtedText;
-    }
-
     public void setOutputtedText(String outputtedText) {
         this.outputtedText.set(outputtedText);
+    }
+
+    public StringProperty outputtedTextProperty() {
+        return outputtedText;
     }
 
     public void appendToOutputtedText(String text) {
