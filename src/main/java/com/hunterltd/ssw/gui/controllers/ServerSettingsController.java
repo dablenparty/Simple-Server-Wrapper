3f728f5bbfc2bdb9aacb1f5e234bebd8834390a3
dablenparty <hdavenport8902@gmail.com>
2021-12-27 14:33:06 -0500

    Bound restart slider to model property

--- src/main/java/com/hunterltd/ssw/gui/controllers/ServerSettingsController.java
+++ src/main/java/com/hunterltd/ssw/gui/controllers/ServerSettingsController.java
@@ -6,10 +6,7 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -29,6 +26,8 @@ public class ServerSettingsController {
     private Button saveButton;
     @FXML
     private Button cancelButton;
+    @FXML
+    private Slider restartIntervalSlider;
 
     public ServerSettingsController(SimpleServerWrapperModel model, MinecraftServer minecraftServer) {
         this.model = model;
@@ -36,6 +35,7 @@ public class ServerSettingsController {
     }
 
     public void initialize() {
+        // General tab
         ObjectProperty<ObservableList<Double>> memoryObjectProperty = new SimpleObjectProperty<>(model.getServerMemory());
         memoryComboBox.itemsProperty().bind(memoryObjectProperty);
         memoryComboBox.setOnAction(actionEvent -> {
@@ -46,6 +46,10 @@ public class ServerSettingsController {
         memoryComboBox.setValue(serverSettings.getMemory());
         memoryProgressBar.setProgress(serverSettings.getMemory() / model.getMaxMemory());
         extraArgsTextField.setText(String.join(" ", serverSettings.getExtraArgs()));
+
+        // Automation tab
+        model.setRestartInterval(serverSettings.getRestartInterval());
+        model.restartIntervalProperty().bind(restartIntervalSlider.valueProperty());
     }
 
     @FXML
@@ -60,6 +64,7 @@ public class ServerSettingsController {
         serverSettings.setMemory(memoryComboBox.getValue());
         String argsFieldText = extraArgsTextField.getText();
         List<String> extraArgs = argsFieldText.isBlank() ? new ArrayList<>() : List.of(argsFieldText.split(" "));
+        serverSettings.setRestartInterval(model.getRestartInterval());
         serverSettings.setExtraArgs(extraArgs);
         serverSettings.writeData();
     }
