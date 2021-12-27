94e23a9bc620eb8cd9979ce706780a921265785d
dablenparty <hdavenport8902@gmail.com>
2021-12-27 13:22:10 -0500

    Added a minecraftServer field

--- src/main/java/com/hunterltd/ssw/gui/controllers/ServerSettingsController.java
+++ src/main/java/com/hunterltd/ssw/gui/controllers/ServerSettingsController.java
@@ -1,6 +1,7 @@
 package com.hunterltd.ssw.gui.controllers;
 
 import com.hunterltd.ssw.gui.model.SimpleServerWrapperModel;
+import com.hunterltd.ssw.minecraft.MinecraftServer;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
@@ -11,13 +12,16 @@ public class ServerSettingsController {
     @FXML
     public ComboBox<Double> memoryComboBox;
     private final SimpleServerWrapperModel model;
+    private final MinecraftServer minecraftServer;
 
-    public ServerSettingsController(SimpleServerWrapperModel model) {
+    public ServerSettingsController(SimpleServerWrapperModel model, MinecraftServer minecraftServer) {
         this.model = model;
+        this.minecraftServer = minecraftServer;
     }
 
     public void initialize() {
         ObjectProperty<ObservableList<Double>> memoryObjectProperty = new SimpleObjectProperty<>(model.getServerMemory());
         memoryComboBox.itemsProperty().bind(memoryObjectProperty);
+        memoryComboBox.setValue(minecraftServer.getServerSettings().getMemory());
     }
 }
