8ce788f310439adf5743156270a013a96a9a7ca3
dablenparty <hdavenport8902@gmail.com>
2021-12-27 13:10:46 -0500

    Binds combo box to model

--- src/main/java/com/hunterltd/ssw/gui/controllers/ServerSettingsController.java
+++ src/main/java/com/hunterltd/ssw/gui/controllers/ServerSettingsController.java
@@ -1,4 +1,23 @@
 package com.hunterltd.ssw.gui.controllers;
 
+import com.hunterltd.ssw.gui.model.SimpleServerWrapperModel;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class ServerSettingsController {
+    @FXML
+    public ComboBox<Double> memoryComboBox;
+    private final SimpleServerWrapperModel model;
+
+    public ServerSettingsController(SimpleServerWrapperModel model) {
+        this.model = model;
+    }
+
+    public void initialize() {
+        ObjectProperty<ObservableList<Double>> memoryObjectProperty = new SimpleObjectProperty<>(model.getServerMemory());
+        memoryComboBox.itemsProperty().bind(memoryObjectProperty);
+    }
 }
