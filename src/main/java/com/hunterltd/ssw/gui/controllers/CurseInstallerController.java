6c59d5e597367915b9b616fd1d4b347198c34f82
dablenparty <hdavenport8902@gmail.com>
2022-01-03 14:54:57 -0500

    Implemented components of the view

--- src/main/java/com/hunterltd/ssw/gui/controllers/CurseInstallerController.java
+++ src/main/java/com/hunterltd/ssw/gui/controllers/CurseInstallerController.java
@@ -1,14 +1,32 @@
 package com.hunterltd.ssw.gui.controllers;
 
 import com.hunterltd.ssw.gui.model.SimpleServerWrapperModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 
 public class CurseInstallerController extends FxController {
+    @FXML
+    private Label installProgressLabel;
+    @FXML
+    private ProgressBar installProgressBar;
+    @FXML
+    private TextField serverFolderTextField;
+    @FXML
+    private TextField modpackTextField;
+
     public CurseInstallerController(SimpleServerWrapperModel model) {
         super(model);
     }
 
     @Override
     public void initialize() {
+
+    }
+
+    @FXML
+    protected void onInstallButtonClicked() {
 
     }
 }
