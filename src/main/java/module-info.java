11f211e41ef0a1a96ec84bbd8f9145930ecb54e4
dablenparty <hdavenport8902@gmail.com>
2022-01-03 16:06:11 -0500

    Added exports and opens statements for curse package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,11 +15,15 @@ module com.hunterltd.ssw {
     opens com.hunterltd.ssw.gui to javafx.fxml;
     opens com.hunterltd.ssw.gui.controllers to javafx.fxml;
     opens com.hunterltd.ssw.minecraft to com.google.gson;
+    opens com.hunterltd.ssw.curse to com.google.gson;
+    opens com.hunterltd.ssw.curse.api to com.google.gson;
+    opens com.hunterltd.ssw.util.events to com.google.gson;
     exports com.hunterltd.ssw.gui;
     exports com.hunterltd.ssw.gui.controllers;
     exports com.hunterltd.ssw.gui.components;
     exports com.hunterltd.ssw.gui.model;
     exports com.hunterltd.ssw.minecraft;
+    exports com.hunterltd.ssw.curse.api;
     exports com.hunterltd.ssw.util;
     exports com.hunterltd.ssw.util.concurrency;
 }
