8a57cddc575bf2abb63c39435438948dc1bd45a5
dablenparty <hdavenport8902@gmail.com>
2022-01-02 20:01:29 -0500

    Added autoScroll boolean property

--- src/main/java/com/hunterltd/ssw/gui/components/AutoScrollTextArea.java
+++ src/main/java/com/hunterltd/ssw/gui/components/AutoScrollTextArea.java
@@ -1,6 +1,25 @@
 package com.hunterltd.ssw.gui.components;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.control.TextArea;
 
 public class AutoScrollTextArea extends TextArea {
+    private final BooleanProperty autoScroll;
+
+    public AutoScrollTextArea() {
+        autoScroll = new SimpleBooleanProperty(true);
+    }
+
+    public boolean isAutoScroll() {
+        return autoScroll.get();
+    }
+
+    public void setAutoScroll(boolean autoScroll) {
+        this.autoScroll.set(autoScroll);
+    }
+
+    public BooleanProperty autoScrollProperty() {
+        return autoScroll;
+    }
 }
