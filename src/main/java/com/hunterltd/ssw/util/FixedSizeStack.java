5104cd3c67d9f33b80fcd32fffcbe45369fb467d
dablenparty <hdavenport8902@gmail.com>
2022-01-03 01:35:58 -0500

    Added documentation and peek method

--- src/main/java/com/hunterltd/ssw/util/FixedSizeStack.java
+++ src/main/java/com/hunterltd/ssw/util/FixedSizeStack.java
@@ -4,25 +4,59 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.List;
 
+/**
+ * A bare-bones stack implementation that holds a fixed number of elements. An {@link ArrayList} is used internally, so
+ * this class is not inherently thread-safe. It is still, by definition, a FILO (First In Last Out) data structure, but
+ * it works by removing the element at the bottom of the stack if the max size has been reached when adding a new
+ * element.
+ * This does not extend the built-in {@link java.util.Stack} class because that implementation is based upon
+ * {@code Vector}'s, which are (by design) thread-safe and have too many functions to override for
+ * adding/removing/inserting elements if I wish to keep my sanity.
+ *
+ * @param <E> Type of elements
+ */
 public class FixedSizeStack<E> {
     private final List<E> elements;
     private final int maxSize;
 
+    /**
+     * Constructs a new {@code FixedSizeStack} with a maximum size of {@code maxSize} elements
+     *
+     * @param maxSize Maximum number of elements
+     */
     public FixedSizeStack(int maxSize) {
         this.maxSize = maxSize;
         elements = new ArrayList<>(maxSize);
     }
 
+    /**
+     * Pushes an element to the top of the stack
+     *
+     * @param item Element to add
+     */
     public void push(E item) {
-        if (elements.size() == maxSize)
-            elements.remove(0);
+        if (elements.size() == maxSize) elements.remove(0);
         elements.add(item);
     }
 
+    /**
+     * Pops an element off the top of the stack
+     *
+     * @return Element on top of the stack
+     */
     public E pop() {
-        if (elements.isEmpty())
-            throw new EmptyStackException();
+        if (elements.isEmpty()) throw new EmptyStackException();
         return elements.remove(elements.size() - 1);
+    }
+
+    /**
+     * Returns, but does not remove, the element at the top of the stack
+     *
+     * @return Element on top of the stack
+     */
+    public E peek() {
+        if (elements.isEmpty()) throw new EmptyStackException();
+        return elements.get(elements.size() - 1);
     }
 
     @Override
