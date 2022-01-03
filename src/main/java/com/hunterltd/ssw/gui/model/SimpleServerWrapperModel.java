4ee31b4e37f795e62eac3220dc21536292cd1177
dablenparty <hdavenport8902@gmail.com>
2022-01-03 00:13:12 -0500

    Removed unused autoScroll property

--- src/main/java/com/hunterltd/ssw/gui/model/SimpleServerWrapperModel.java
+++ src/main/java/com/hunterltd/ssw/gui/model/SimpleServerWrapperModel.java
@@ -14,7 +14,6 @@ import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
 public class SimpleServerWrapperModel {
-    private final BooleanProperty autoScroll;
     private final BooleanProperty serverRunning;
     private final StringProperty serverPath;
     private final StringProperty commandText;
@@ -32,19 +31,6 @@ public class SimpleServerWrapperModel {
         serverPath = new SimpleStringProperty();
         serverRunning = new SimpleBooleanProperty(false);
         commandText = new SimpleStringProperty("");
-        autoScroll = new SimpleBooleanProperty(true);
-    }
-
-    public boolean isAutoScroll() {
-        return autoScroll.get();
-    }
-
-    public void setAutoScroll(boolean autoScroll) {
-        this.autoScroll.set(autoScroll);
-    }
-
-    public BooleanProperty autoScrollProperty() {
-        return autoScroll;
     }
 
     public String getCommandText() {
