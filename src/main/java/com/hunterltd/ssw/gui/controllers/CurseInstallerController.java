e9dcb4a29ea539c86783a4bcfb9ffb8f2cc5d9b6
dablenparty <hdavenport8902@gmail.com>
2022-01-03 14:38:36 -0500

    Created curse-installer-view.fxml with accompanying CurseInstallerController.java

---
+++ src/main/java/com/hunterltd/ssw/gui/controllers/CurseInstallerController.java
@@ -0,0 +1,14 @@
+package com.hunterltd.ssw.gui.controllers;
+
+import com.hunterltd.ssw.gui.model.SimpleServerWrapperModel;
+
+public class CurseInstallerController extends FxController {
+    public CurseInstallerController(SimpleServerWrapperModel model) {
+        super(model);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
