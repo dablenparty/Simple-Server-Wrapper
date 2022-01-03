e041ff13be18b9e6167fcf9834b9ee6858dec314
dablenparty <hdavenport8902@gmail.com>
2022-01-03 01:19:06 -0500

    Added push and pop

--- src/main/java/com/hunterltd/ssw/util/FixedSizeStack.java
+++ src/main/java/com/hunterltd/ssw/util/FixedSizeStack.java
@@ -1,11 +1,26 @@
 package com.hunterltd.ssw.util;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 public class FixedSizeStack<E> {
     private final ArrayList<E> elements;
+    private final int maxSize;
 
-    public FixedSizeStack(int size) {
-        elements = new ArrayList<>(size + 1);
+    public FixedSizeStack(int maxSize) {
+        this.maxSize = maxSize;
+        elements = new ArrayList<>(maxSize + 1);
+    }
+
+    public void push(E item) {
+        if (elements.size() == maxSize)
+            elements.remove(0);
+        elements.add(item);
+    }
+
+    public E pop() {
+        if (elements.isEmpty())
+            throw new EmptyStackException();
+        return elements.remove(elements.size() - 1);
     }
 }
