69d9f52b3e291f9e4d0db478e328dd577efe53de
dablenparty <hdavenport8902@gmail.com>
2022-01-03 23:03:42 -0500

    Added error dialogs

--- src/main/java/com/hunterltd/ssw/gui/controllers/CurseInstallerController.java
+++ src/main/java/com/hunterltd/ssw/gui/controllers/CurseInstallerController.java
@@ -6,6 +6,7 @@ import com.hunterltd.ssw.gui.model.SimpleServerWrapperModel;
 import com.hunterltd.ssw.util.concurrency.NamedExecutorService;
 import com.hunterltd.ssw.util.concurrency.ThreadUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
@@ -40,27 +41,42 @@ public class CurseInstallerController extends FxController {
     }
 
     @FXML
-    protected void onInstallButtonClicked() throws IOException {
+    protected void onInstallButtonClicked() {
         SimpleServerWrapperModel model = getInternalModel();
 
-        // TODO make error dialog if something goes wrong here (the modpack class is auto-closeable)
-        CurseModpack modpack = CurseModpack.createCurseModpack(modpackZipFile);
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        service.submit(() -> modpack.install(Path.of(model.getServerPath()).getParent()));
-        NamedExecutorService namedExecutorService = new NamedExecutorService("Modpack Install Service", service);
-        modpack.on("download", args -> {
-            CurseAddon mod = (CurseAddon) args[0];
-            int modNumber = (int) args[1];
-            int filesLength = (int) args[2];
-            ThreadUtils.runOnFxThread(() -> {
-                installProgressLabel.setText(String.format("Mod %d/%d: %s", modNumber, filesLength, mod));
-                installProgressBar.setProgress((double) modNumber / filesLength);
+        try (CurseModpack modpack = CurseModpack.createCurseModpack(modpackZipFile)) {
+            ExecutorService service = Executors.newSingleThreadExecutor();
+            service.submit(() -> modpack.install(Path.of(model.getServerPath()).getParent()));
+            NamedExecutorService namedExecutorService = new NamedExecutorService("Modpack Install Service", service);
+            modpack.on("download", args -> {
+                CurseAddon mod = (CurseAddon) args[0];
+                int modNumber = (int) args[1];
+                int filesLength = (int) args[2];
+                ThreadUtils.runOnFxThread(() -> {
+                    installProgressLabel.setText(String.format("Mod %d/%d: %s", modNumber, filesLength, mod));
+                    installProgressBar.setProgress((double) modNumber / filesLength);
+                });
             });
-        });
-        modpack.on("done", args -> {
-            ThreadUtils.runOnFxThread(() -> installProgressLabel.setText("Done!"));
-            ThreadUtils.tryShutdownNamedExecutorService(namedExecutorService);
-        });
+            modpack.on("done", args -> {
+                ThreadUtils.runOnFxThread(() -> installProgressLabel.setText("Done!"));
+                ThreadUtils.tryShutdownNamedExecutorService(namedExecutorService);
+            });
+            modpack.on("error", args -> {
+                Exception exception = (Exception) args[0];
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(exception.toString());
+                alert.setContentText(exception.getLocalizedMessage());
+                alert.show();
+            });
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(e.toString());
+            alert.setContentText(e.getLocalizedMessage());
+            alert.show();
+            e.printStackTrace();
+        }
     }
 
     @FXML
