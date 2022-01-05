54fb161427dda73748bbf33461440c254c421c28
dablenparty <hdavenport8902@gmail.com>
2022-01-05 13:27:36 -0500

    Made APP_DATA_PATH a final public field that is statically initialized

--- src/main/java/com/hunterltd/ssw/util/os/SpecialFolderFactory.java
+++ src/main/java/com/hunterltd/ssw/util/os/SpecialFolderFactory.java
@@ -7,30 +7,27 @@ import java.util.Locale;
 import java.util.Properties;
 
 public class SpecialFolderFactory {
-    private static Path appData = null;
+    public static final Path APP_DATA_PATH;
 
-    public static Path getAppDataPath() {
-        if (appData == null) {
-            Properties mavenProperties = MavenUtils.getMavenProperties();
-            String artifactId = mavenProperties.getProperty("artifactId");
+    static {
+        Properties mavenProperties = MavenUtils.MAVEN_PROPERTIES;
+        String artifactId = mavenProperties.getProperty("artifactId");
 
-            String osName = System.getProperty("os.name");
-            osName = osName.toLowerCase(Locale.ROOT);
-            String userHome = System.getProperty("user.home");
+        String osName = System.getProperty("os.name");
+        osName = osName.toLowerCase(Locale.ROOT);
+        String userHome = System.getProperty("user.home");
 
-            Path result;
-            if (osName.startsWith("windows"))
-                result = Path.of(userHome, "AppData", "Local", artifactId);
-            else if (osName.startsWith("mac"))
-                result = Path.of(userHome, "Library", "Application Support", artifactId);
-            else
-                result = Path.of(userHome, ".config", artifactId);
-            appData = result;
-        }
-        return appData;
+        Path result;
+        if (osName.startsWith("windows"))
+            result = Path.of(userHome, "AppData", "Local", artifactId);
+        else if (osName.startsWith("mac"))
+            result = Path.of(userHome, "Library", "Application Support", artifactId);
+        else
+            result = Path.of(userHome, ".config", artifactId);
+        APP_DATA_PATH = result;
     }
 
     public static void main(String[] args) {
-        System.out.println(getAppDataPath());
+        System.out.println(APP_DATA_PATH);
     }
 }
