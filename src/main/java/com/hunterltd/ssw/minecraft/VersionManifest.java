7e1dc0d2f0725d252072eb9f67669acb8627545e
dablenparty <hdavenport8902@gmail.com>
2022-01-05 14:23:19 -0500

    Added methods to parse and deserialize manifest file

--- src/main/java/com/hunterltd/ssw/minecraft/VersionManifest.java
+++ src/main/java/com/hunterltd/ssw/minecraft/VersionManifest.java
@@ -1,7 +1,15 @@
 package com.hunterltd.ssw.minecraft;
 
+import com.google.gson.*;
+import com.hunterltd.ssw.util.serial.JsonUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.URL;
-import java.time.ZonedDateTime;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Set;
 
 public class VersionManifest {
@@ -12,12 +20,40 @@ public class VersionManifest {
 
     public static class MinecraftVersion {
         private String id;
-        private String type;
+        private VersionType type;
         private URL url;
-        private ZonedDateTime time;
-        private ZonedDateTime releaseTime;
+        private String time;
+        private String releaseTime;
+    }
+
+    public enum VersionType {
+        RELEASE,
+        SNAPSHOT;
+
+        public static VersionType parseString(String typeString) {
+            return switch (typeString) {
+                case "release" -> VersionType.RELEASE;
+                case "snapshot" -> VersionType.SNAPSHOT;
+                default -> null;
+            };
+        }
+    }
+
+    private static class VersionTypeDeserializer implements JsonDeserializer<VersionType> {
+        @Override
+        public VersionType deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            return VersionType.parseString(jsonElement.getAsString());
+        }
     }
 
     private Latest latest;
     private Set<MinecraftVersion> versions;
+
+    public static VersionManifest parseManifestFile(Path manifestPath) throws IOException {
+        GsonBuilder builder = JsonUtils.GSON_BUILDER;
+        builder.registerTypeAdapter(VersionType.class, new VersionTypeDeserializer());
+        Gson gson = builder.create();
+        InputStream manifestStream = Files.newInputStream(manifestPath);
+        return gson.fromJson(new InputStreamReader(manifestStream), VersionManifest.class);
+    }
 }
