375b832910ba519dcf9e50fdb62d0d212d5846cf
dablenparty <hdavenport8902@gmail.com>
2022-01-05 13:14:58 -0500

    Caches Maven properties so they don't have to be loaded more than once

--- src/main/java/com/hunterltd/ssw/util/MavenUtils.java
+++ src/main/java/com/hunterltd/ssw/util/MavenUtils.java
@@ -7,12 +7,16 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class MavenUtils {
+    private static Properties mavenProperties = null;
+
     public static Properties getMavenProperties() {
-        Properties mavenProperties = new Properties();
-        try (InputStream resourceStream = SswClientCli.class.getClassLoader().getResourceAsStream("project.properties")) {
-            mavenProperties.load(resourceStream);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (mavenProperties == null) {
+            mavenProperties = new Properties();
+            try (InputStream resourceStream = SswClientCli.class.getClassLoader().getResourceAsStream("project.properties")) {
+                mavenProperties.load(resourceStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return mavenProperties;
     }
