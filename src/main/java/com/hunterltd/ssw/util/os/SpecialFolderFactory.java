522b06c0731af589551adee5f8669ec13e6cb065
dablenparty <hdavenport8902@gmail.com>
2022-01-05 13:24:08 -0500

    Implemented static getAppDataPath method for getting the apps data path

--- src/main/java/com/hunterltd/ssw/util/os/SpecialFolderFactory.java
+++ src/main/java/com/hunterltd/ssw/util/os/SpecialFolderFactory.java
@@ -1,4 +1,36 @@
 package com.hunterltd.ssw.util.os;
 
+import com.hunterltd.ssw.util.MavenUtils;
+
+import java.nio.file.Path;
+import java.util.Locale;
+import java.util.Properties;
+
 public class SpecialFolderFactory {
+    private static Path appData = null;
+
+    public static Path getAppDataPath() {
+        if (appData == null) {
+            Properties mavenProperties = MavenUtils.getMavenProperties();
+            String artifactId = mavenProperties.getProperty("artifactId");
+
+            String osName = System.getProperty("os.name");
+            osName = osName.toLowerCase(Locale.ROOT);
+            String userHome = System.getProperty("user.home");
+
+            Path result;
+            if (osName.startsWith("windows"))
+                result = Path.of(userHome, "AppData", "Local", artifactId);
+            else if (osName.startsWith("mac"))
+                result = Path.of(userHome, "Library", "Application Support", artifactId);
+            else
+                result = Path.of(userHome, ".config", artifactId);
+            appData = result;
+        }
+        return appData;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getAppDataPath());
+    }
 }
