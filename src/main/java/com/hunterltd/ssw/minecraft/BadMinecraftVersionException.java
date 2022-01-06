61f84fcd9646cef9703a163f6000659590396f80
dablenparty <hdavenport8902@gmail.com>
2022-01-05 22:54:30 -0500

    Removed unused exception

--- src/main/java/com/hunterltd/ssw/minecraft/BadMinecraftVersionException.java
+++
@@ -1,7 +0,0 @@
-package com.hunterltd.ssw.minecraft;
-
-public class BadMinecraftVersionException extends Exception {
-    public BadMinecraftVersionException(String versionString) {
-        super(versionString + " is not a valid Minecraft version");
-    }
-}
