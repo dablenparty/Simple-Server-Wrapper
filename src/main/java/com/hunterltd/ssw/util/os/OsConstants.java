87cb2aeec163c5b29c979624448025f1ff5eb248
dablenparty <hdavenport8902@gmail.com>
2022-01-07 14:29:05 -0500

    Changed USER_HOME from Path to String

--- src/main/java/com/hunterltd/ssw/util/os/OsConstants.java
+++ src/main/java/com/hunterltd/ssw/util/os/OsConstants.java
@@ -4,5 +4,5 @@ import java.nio.file.Path;
 
 public class OsConstants {
     public static final String OS_NAME = System.getProperty("os.name");
-    public static final Path USER_HOME = Path.of(System.getProperty("user.home"));
+    public static final String USER_HOME = System.getProperty("user.home");
 }
