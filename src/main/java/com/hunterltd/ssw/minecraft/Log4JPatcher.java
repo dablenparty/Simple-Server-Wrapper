8f09633985975ea248eeef025563468dab255999
dablenparty <hdavenport8902@gmail.com>
2022-01-08 12:13:59 -0500

    Added constructor based on server version

--- src/main/java/com/hunterltd/ssw/minecraft/Log4JPatcher.java
+++ src/main/java/com/hunterltd/ssw/minecraft/Log4JPatcher.java
@@ -1,4 +1,62 @@
 package com.hunterltd.ssw.minecraft;
 
-public class Log4JPatcher {
+import java.net.MalformedURLException;
+import java.net.URL;
+
+class Log4JPatcher {
+    private static final MinecraftVersion ONE_EIGHTEEN_ONE = MinecraftVersion.of("1.18.1");
+    private static final MinecraftVersion ONE_SEVENTEEN = MinecraftVersion.of("1.17");
+    private static final MinecraftVersion ONE_TWELVE = MinecraftVersion.of("1.12");
+    private static final MinecraftVersion ONE_SEVEN = MinecraftVersion.of("1.7");
+    private final URL patchFileUrl;
+    private final String jvmArgs;
+    private final MinecraftServer minecraftServer;
+
+    Log4JPatcher(MinecraftServer server) {
+        minecraftServer = server;
+        MinecraftVersion serverVersion = server.getServerSettings().getVersion();
+
+        String args = "";
+        URL url = null;
+        // versions 1.18.1 and later are patched, versions 1.7 and earlier are not affected
+        if (versionInRange(serverVersion, ONE_SEVENTEEN, ONE_EIGHTEEN_ONE)) {
+            args = "-Dlog4j2.formatMsgNoLookups=true";
+        } else if (versionInRange(serverVersion, ONE_TWELVE, ONE_SEVENTEEN)) {
+            try {
+                url = new URL("https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml");
+                args = "-Dlog4j.configurationFile=log4j2_112-116.xml";
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else if (versionInRange(serverVersion, ONE_SEVEN, ONE_TWELVE)) {
+            try {
+                url = new URL("https://launcher.mojang.com/v1/objects/4bb89a97a66f350bc9f73b3ca8509632682aea2e/log4j2_17-111.xml");
+                args = "-Dlog4j.configurationFile=log4j2_17-111.xml";
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        jvmArgs = args;
+        patchFileUrl = url;
+    }
+
+    public URL getPatchFileUrl() {
+        return patchFileUrl;
+    }
+
+    public String getJvmArgs() {
+        return jvmArgs;
+    }
+
+    /**
+     * Checks whether a {@code MinecraftVersion} was released between two other versions.
+     *
+     * @param version version to compare
+     * @param start   start of the range (inclusive)
+     * @param end     end of the range (exclusive)
+     * @return {@code true} if {@code version} is within the range. {@code false} otherwise
+     */
+    private boolean versionInRange(MinecraftVersion version, MinecraftVersion start, MinecraftVersion end) {
+        return version.compareTo(start) >= 0 && version.compareTo(end) < 0;
+    }
 }
