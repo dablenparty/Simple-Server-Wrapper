0975265cf22fb940e40ec738023a9af99f4e0df6
dablenparty <hdavenport8902@gmail.com>
2022-01-08 00:37:46 -0500

    Added documentation to GsonExclude.java

--- src/main/java/com/hunterltd/ssw/util/serial/GsonExclude.java
+++ src/main/java/com/hunterltd/ssw/util/serial/GsonExclude.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Excludes a field/class from serialization/deserialization by Gson
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface GsonExclude {
