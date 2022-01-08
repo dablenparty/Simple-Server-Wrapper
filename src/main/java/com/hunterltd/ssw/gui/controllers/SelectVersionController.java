f9edf4a6d191a0f0f35d698b3a47a91cf3b63e75
dablenparty <hdavenport8902@gmail.com>
2022-01-08 18:10:32 -0500

    Created select-version-view.fxml and SelectVersionController.java

---
+++ src/main/java/com/hunterltd/ssw/gui/controllers/SelectVersionController.java
@@ -0,0 +1,14 @@
+package com.hunterltd.ssw.gui.controllers;
+
+import com.hunterltd.ssw.gui.model.SimpleServerWrapperModel;
+
+public class SelectVersionController extends FxController {
+    public SelectVersionController(SimpleServerWrapperModel model) {
+        super(model);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
