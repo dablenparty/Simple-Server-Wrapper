2f68c697c42093d41b5ddbad16f05b0d7908bbf6
dablenparty <hdavenport8902@gmail.com>
2022-01-08 00:26:38 -0500

    Added documentation to MinecraftVersion.java

--- src/main/java/com/hunterltd/ssw/minecraft/MinecraftVersion.java
+++ src/main/java/com/hunterltd/ssw/minecraft/MinecraftVersion.java
@@ -5,6 +5,9 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+/**
+ * Represents a version of Minecraft as seen in {@code version_manifest_v2.json}
+ */
 public class MinecraftVersion implements Comparable<MinecraftVersion> {
     private String id;
     private VersionManifestV2.VersionType type;
@@ -14,6 +17,16 @@ public class MinecraftVersion implements Comparable<MinecraftVersion> {
     private String sha1;
     private short complianceLevel;
 
+    /**
+     * Finds the {@code MinecraftVersion} whose ID matches the {@code versionString} parameter.
+     * <p>
+     * Note that because the list of versions is sorted in descending order by release time, older versions will take
+     * longer to find.
+     *
+     * @param versionString Minecraft version string (e.g., {@code 1.18.1})
+     * @return {@code MinecraftVersion} instance whose ID matches {@code versionString}, or {@code null} if such an
+     * instance could not be found
+     */
     public static MinecraftVersion of(String versionString) {
         List<MinecraftVersion> versions = VersionManifestV2.INSTANCE.getVersions();
         for (MinecraftVersion version : versions) {
@@ -58,6 +71,7 @@ public class MinecraftVersion implements Comparable<MinecraftVersion> {
 
     @Override
     public int compareTo(MinecraftVersion o) {
+        // sorts in descending order, meaning the most recent releases are at the start (or have lower indices)
         ZonedDateTime thisTime = ZonedDateTime.parse(this.releaseTime, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
         ZonedDateTime otherTime = ZonedDateTime.parse(o.releaseTime, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
         return otherTime.compareTo(thisTime);
